002a31506b61b43741fd3b95edc06a53ec3408f4
Aryan Sahu <sahuaryan666@gmail.com>
2026-03-29 12:20:38 +0530

    changed folder directory

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from app.schema import CarFeatures,PredictionResponse
-from app.model import predict_price,load_artifacts
+from schema import CarFeatures,PredictionResponse
+from model import predict_price,load_artifacts
 from fastapi.middleware.cors import CORSMiddleware
 
 
